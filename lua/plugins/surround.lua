55e64d36c38630a1207b86d83870f156c3c5f0e8
Ali Barış Gültekin <alibarisgultekin@gmail.com>
2025-11-30 14:30:49 +0300

    Fix surround plugin

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,45 +1,41 @@
 return {
   {
     "kylechui/nvim-surround",
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({
-        keymaps = {
-          insert = "<C-g>s",
-          insert_line = "<C-g>S",
-          normal = "ys",
-          normal_cur = "yss",
-          normal_line = "yS",
-          normal_cur_line = "ySS",
-          visual = "S",
-          visual_line = "gS",
-          delete = "ds",
-          change = "cs",
+    version = "*",
+    keys = {
+      { "ys", mode = "n" },
+      { "yss", mode = "n" },
+      { "yS", mode = "n" },
+      { "ySS", mode = "n" },
+      { "S", mode = "x" },
+      { "gS", mode = "x" },
+      { "ds", mode = "n" },
+      { "cs", mode = "n" },
+    },
+    opts = {
+      keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "ys",
+        normal_cur = "yss",
+        normal_line = "yS",
+        normal_cur_line = "ySS",
+        visual = "S",
+        visual_line = "gS",
+        delete = "ds",
+        change = "cs",
+      },
+      surrounds = {
+        -- Custom tag prompt (use St then type tag name)
+        ["t"] = {
+          add = function()
+            local tag = require("nvim-surround.config").get_input("Tag name: ")
+            return { { "<" .. tag .. ">" }, { "</" .. tag .. ">" } }
+          end,
         },
-        surrounds = {
-          -- Custom surrounds for JSX/HTML
-          ["t"] = {
-            add = function()
-              local tag = require("nvim-surround.config").get_input("Tag name: ")
-              return { { "<" .. tag .. ">" }, { "</" .. tag .. ">" } }
-            end,
-          },
-          ["T"] = {
-            add = function()
-              local tag = require("nvim-surround.config").get_input("Tag name: ")
-              local attrs = require("nvim-surround.config").get_input("Attributes: ")
-              if attrs ~= "" then
-                return { { "<" .. tag .. " " .. attrs .. ">" }, { "</" .. tag .. ">" } }
-              end
-              return { { "<" .. tag .. ">" }, { "</" .. tag .. ">" } }
-            end,
-          },
-          -- Quick div wrapper
-          ["d"] = { "<div>", "</div>" },
-          -- Quick span wrapper
-          ["s"] = { "<span>", "</span>" },
-        },
-      })
-    end,
+        -- Quick div wrapper (Sd in visual mode)
+        ["d"] = { { "<div>" }, { "</div>" } },
+      },
+    },
   },
 }
